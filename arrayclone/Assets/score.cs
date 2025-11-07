100340ba3c2b0acc87c1b801a0de0cc707bbfe7a
princessariel3306-lab <princessariel3306@gmail.com>
2025-11-07 09:02:27 -0500

    add score ui

--- arrayclone/Assets/score.cs
+++ arrayclone/Assets/score.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class score : MonoBehaviour
 {
+
+    public TextMeshProUGUI seaweed;
+    public int Rock = 0;
     // Start is called before the first frame update
+
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+
+
+    }
     void Update()
     {
-        
+
     }
+
+    public void UpdateScore()
+    {
+        seaweed.text = "" + Rock.ToString();
+        Rock += 1;
+    }
+   
+
+
+
 }
