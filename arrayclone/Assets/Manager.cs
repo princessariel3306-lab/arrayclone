cff540424055ae91b3ff0990dc8e2f72e59a25fa
princessariel3306-lab <princessariel3306@gmail.com>
2025-11-06 19:15:29 -0500

    Update Manager.cs

--- arrayclone/Assets/Manager.cs
+++ arrayclone/Assets/Manager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Text Result;
+    public Image AiChoice;
+    public string[] Choices;
+    public Sprite Rock, Paper, Scissors;
+    public void Play(string myChoice)
+    { string randomChoice = Choices[Random.Range(0, Choices.Length)];
+        switch (randomChoice)
+        {
+            case "Rock":
+                switch (myChoice)
+                {
+                    case "Rock":
+                        Result.text = "tie ";
+                        break;
+                    case "Paper":
+                        Result.text = "lose ";
+                        break;
+                    case "Scissor":
+                        Result.text = "win ";
+                        break;
+                }
+                AiChoice.sprite = Rock;
+                break;
+            case "Paper":
+                AiChoice.sprite = Paper;
+                switch (myChoice)
+                {
+                    case "Rock":
+                        Result.text = "win";
+                        break;
+                    case "Paper":
+                        Result.text = "tie ";
+                        break;
+                    case "Scissor":
+                        Result.text = "lose";
+                        break;
+                }
+                break;
+            case "Scissor":
+                AiChoice.sprite = Scissors;
+                switch (myChoice)
+                {
+                    case "Rock":
+                        Result.text = "lose";
+                        break;
+                    case "Paper":
+                        Result.text = "win";
+                        break;
+                    case "Scissor":
+                        Result.text = "tie";
+                        break;
+                }
+                break;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
+
+
+
+
